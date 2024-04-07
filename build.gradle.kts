16e1d1a89c535ccfb90d82e445429e62f58354fd
Yulia Lener <yulias.docs@gmail.com>
2024-04-07 17:18:06 +0700

    junit4

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,1 +1,19 @@
-git
+plugins {
+    id("java")
+}
+
+group = "ru.netology"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("junit:junit:4.13")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
